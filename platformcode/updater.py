4143c743683e5781897daf6cbb489dd026f5ca8c
aandroide <14141574+aandroide@users.noreply.github.com>
2025-02-18 00:13:55 +0100

    Update updater.py

--- platformcode/updater.py
+++ platformcode/updater.py
@@ -35,7 +35,7 @@ trackingFile = "last_commit.txt"
 def wizard():
     name='artic'
     #url= 'https://www.dropbox.com/scl/fi/wa72e0hkspev5wonx7bpq/test.zip?rlkey=rl2mp1awarn513xjee9wzwo8m&st=y82y7w2d&dl=1'
-    url= 'https://www.dropbox.com/scl/fi/epqt5iinnzstk48ey0rcb/artic.zip?rlkey=3udxp42iylzt3d5x36h2gbjg2&st=j7ys0124&dl=1'
+    url= 'https://www.dropbox.com/scl/fi/epqt5iinnzstk48ey0rcb/artic.zip?rlkey=3udxp42iylzt3d5x36h2gbjg2&st=z0mqcylv&dl=1'
     path = xbmcvfs.translatePath(os.path.join('special://home/addons','packages'))
     dp = xbmcgui.DialogProgress()
     dp.create("LO SCIENZIATO PAZZO","In Download \n\n Attendere Prego")
